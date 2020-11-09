5cdd31c49966f33d747f8024f688b2e7a7915930
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-11-10 00:22:57 +0100

    Create create-account.svelte

---
+++ src/routes/create-account.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { callApi } from "../utils/api.js";
+    import { onMount } from "svelte";
+    import { goto } from "@sapper/app";
+    let account;
+    let email;
+    let link;
+    let linkId;
+    
+    onMount(async ()=>{
+    account = await callApi("get", "/account");
+    if(account.user) goto("/")
+    })
+    async function onClick(){
+        try{
+        linkId = new URL(link)
+        linkId = linkId.pathname.split('/')[2]
+        await callApi("post", `/auth/createAccount?email=${email}&linkId=${linkId}`)
+        goto('/')
+        }catch(err){
+            console.log(err)
+        }
+    }
+    
+</script>
+<svelte:head>
+<title>Create account | Winhalla</title>
+</svelte:head>
+<div class="p-8">
+    <h2>Email</h2>
+    <input size=100 id="test" bind:value={email} class="text-black p-1">
+    <h2 class="pt-8">Friend link</h2>
+    <input size=100 bind:value={link} class="text-black p-1">
+    <br>
+    <button on:click={onClick} class="px-4 py-1 mt-4 bg-primary rounded">Create account</button>
+    {linkId}
+</div>

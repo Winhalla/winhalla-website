8689282c24c7bcda0a7c0cc1a6dbf142bcfd4c2f
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-11-18 16:18:53 +0100

    noobz

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create({
     withCredentials: true,
     baseURL: "https://api-dot-winhalla.appspot.com",
-    headers: {
-        "Sec-Fetch-Site":"same-site"
-      }
 });
 
 const callApi = async (method, url, data) => {

4a2d4778ccc454a019bbee496dde574de06e6a16
Felons <slavikzhyhota@gmail.com>
2020-11-18 19:21:29 +0100

    Finished create account page. (not responsive)

--- src/routes/create-account.svelte
+++ src/routes/create-account.svelte
@@ -6,33 +6,43 @@
     let email;
     let link;
     let linkId;
-    let validLink;
-    let valid = false;
+    let validLink = null;
+    let validEmail = null;
 
     const onKeyPressLink = () => {
         setTimeout(async () => {
-            try {
-                linkId = new URL(link);
-                linkId = linkId.pathname.split("/")[2];
-                if (linkId.length == 24) {
-                    const testLink = await callApi("get", `/getLink/${linkId}`);
-                    if (testLink) validLink = true;
-                } else {
+            if (link.length > 0) {
+                try {
+                    linkId = new URL(link);
+                    linkId = linkId.pathname.split("/")[2];
+                    if (linkId.length == 24) {
+                        const testLink = await callApi(
+                            "get",
+                            `/getLink/${linkId}`
+                        );
+                        if (testLink) validLink = true;
+                    } else {
+                        validLink = null;
+                    }
+                } catch (err) {
                     validLink = false;
                 }
-            } catch (err) {
-                validLink = false;
+            } else {
+                validLink = null;
             }
         }, 1);
     };
 
     const onKeyPressEmail = () => {
         setTimeout(() => {
-            let regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm;
-            let exec = regex.exec(email);
-            console.log(exec);
-            if (exec) valid = true;
-            else valid = false;
+            if (email.length > 0) {
+                let regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm;
+                let exec = regex.exec(email);
+                if (exec) validEmail = true;
+                else validEmail = false;
+            } else {
+                validEmail = null;
+            }
         }, 1);
     };
 
@@ -58,7 +68,7 @@
 
 <style>
     input {
-        @apply text-background pl-2 text-lg py-1 bg-font w-96 rounded border-2 border-transparent;
+        @apply text-background pl-2 text-lg py-1 bg-font w-96 rounded border-2;
     }
 
     button:disabled {
@@ -87,16 +97,17 @@
 <div class="flex items-center justify-center h-screen-70">
     <div class="flex flex-col items-center">
         <h1 class="text-6xl mb-8">Create your account</h1>
-        <div class="mx-auto card py-8 px-10">
+        <div class="mx-auto card pt-8 pb-11 px-10">
             <p class="input-header">Email</p>
             <div>
                 <input
                     on:keydown={onKeyPressEmail}
                     type="email"
                     bind:value={email}
+                    class:border-legendary={validEmail == false}
                     class="input-style focus:outline-none focus:border-primary" />
 
-                {#if valid}
+                {#if validEmail}
                     <div class="flex items-center">
                         <svg
                             class="fill-current text-green w-4 check"
@@ -105,7 +116,7 @@
                                 d="m0 10.909 4.364-4.364 8.727 8.727 15.273-15.273 4.364 4.364-19.636 19.636z" /></svg>
                         <p class="text-green info">VALID EMAIL</p>
                     </div>
-                {:else}
+                {:else if validEmail == false}
                     <p class="text-legendary info ">INVALID EMAIL</p>
                 {/if}
             </div>
@@ -115,16 +126,24 @@
                 <input
                     on:keydown={onKeyPressLink}
                     bind:value={link}
+                    class:border-legendary={validLink == false}
                     class="input-style focus:outline-none focus:border-primary" />
                 {#if validLink}
-                    <p class="text-green info">VALID INPUT</p>
-                {:else}
+                    <div class="flex items-center">
+                        <svg
+                            class="fill-current text-green w-4 check"
+                            viewBox="0 0 33 24"
+                            xmlns="http://www.w3.org/2000/svg"><path
+                                d="m0 10.909 4.364-4.364 8.727 8.727 15.273-15.273 4.364 4.364-19.636 19.636z" /></svg>
+                        <p class="text-green info">VALID INPUT</p>
+                    </div>
+                {:else if validLink == false}
                     <p class="text-legendary info">INVALID INPUT</p>
                 {/if}
             </div>
         </div>
         <button
-            disabled={!valid}
+            disabled={!validEmail}
             on:click={onClick}
             class="button button-brand mt-6">
             Create account

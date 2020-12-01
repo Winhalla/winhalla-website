b340bcba86e62fdf7a21da2546aa8ec7427c8105
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-12-01 18:35:05 +0100

    Update test.svelte

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,12 +1,13 @@
 <script>
     import io from "socket.io-client";
+    import { apiUrl } from "../utils/config";
 
     let variable;
     let update = (update) => {
         variable = update;
         console.log(update)
     };
-    let socket = io("http://localhost:9000");
+    let socket = io(apiUrl+":9000");
     socket.on("test", update);
 
 

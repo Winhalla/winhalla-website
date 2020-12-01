410726e7dcedceeede48c0454287193dab86f29b
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-12-01 17:37:03 +0100

    added socketio

---
+++ src/routes/test.svelte
@@ -0,0 +1,16 @@
+<script>
+    import io from "socket.io-client";
+
+    let variable;
+    let update = (update) => {
+        variable = update;
+        console.log(update)
+    };
+    let socket = io("http://localhost:9000");
+    socket.on("test", update);
+
+
+</script>
+<div class="m-40">
+    {variable}
+</div>

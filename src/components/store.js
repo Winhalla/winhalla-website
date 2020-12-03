7a713a3469dd098476ba02ac42c4c287bd58769e
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-12-04 00:56:31 +0100

    changed api error system to match new store system

--- src/components/store.js
+++ src/components/store.js
@@ -1,11 +1,15 @@
 import { writable } from "svelte/store";
 import { getUser } from "../utils/api.js";
 
-let counter = writable({content: {  },refresh:false});
+let counter;
+    counter = writable({ content: getUser(), refresh: false });
+    //counter = writable({ content: "err", refresh: false });
 
-counter.subscribe(async (value)=>{
-    if (value.refresh === true){
-        counter.set({content:await getUser(),refresh:false})
+
+
+counter.subscribe(async (value) => {
+    if (value.refresh === true) {
+        counter.set({ content: await getUser(), refresh: false });
     }
-})
+});
 export { counter };

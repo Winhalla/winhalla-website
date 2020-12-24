97c9ba557ed50155e3bd1c02e0bfe52e08226bfe
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-12-24 22:08:34 +0100

    build

---
+++ __sapper__/build/client/help.71b62812.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as e,q as a,d as o,n as c}from"./client.09fcf86f.js";function i(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-chgju"]',document.head).forEach(o),this.h()},h(){document.title="How it works | Winhalla"},m:c,p:c,i:c,o:c,d:c}}export default class extends t{constructor(t){super(),s(this,t,null,i,e,{})}}
---
+++ __sapper__/build/client/privacy.dbf62c9c.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as a,q as e,d as c,n as i}from"./client.09fcf86f.js";function l(s){return{c(){this.h()},l(s){e('[data-svelte="svelte-88sy1r"]',document.head).forEach(c),this.h()},h(){document.title="Privacy policy | Winhalla"},m:i,p:i,i:i,o:i,d:i}}export default class extends s{constructor(s){super(),t(this,s,null,l,a,{})}}

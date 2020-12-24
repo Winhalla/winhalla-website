15219f957a117ff575d57e54265c33579219f5bc
LoustuCraft <48993175+LoustuCraft@users.noreply.github.com>
2020-12-24 16:24:21 +0100

    changed some things to notifications to have real redirects

--- src/components/Quests.svelte
+++ src/components/Quests.svelte
@@ -37,6 +37,7 @@
             days = Math.floor(hours / 24);
 
             hours = hours - days * 24;
+            hours = hours < 10 ? "0" + hours : hours;
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
 

4f2ab2a39c6574ce07dbaccdd07df9a7c7c703ef
23felons23 <slavikzhyhota@gmail.com>
2021-01-10 16:55:24 +0100

    Polls now work correctly

--- src/components/Navigation/Nav.svelte
+++ src/components/Navigation/Nav.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { onMount, onDestroy } from "svelte";
+    import { onDestroy, onMount } from "svelte";
     import { clickOutside } from "../../utils/clickOutside";
 
     import NavAccount from "./NavAccount.svelte";
@@ -16,17 +16,22 @@
     export let isScrolling;
     let isNavbarOpen;
     let isUserLoggedIn;
-    let userCoins;
+
+
     let informations;
     let poll;
     let notificationsObj = {};
+
     let user;
+    let userCoins;
+
     let firstLoad = true;
     let offline;
     let loaded = false;
 
     async function calculateProperties(value) {
         const tempUserData = await value;
+
         if (tempUserData.offline) offline = true;
         if (tempUserData instanceof Error) {
             if (tempUserData.response) if (tempUserData.response.status === 503) goto("/status");
@@ -53,8 +58,10 @@
 
         await calculateProperties(user);
     };
+
     const unsubscribe = counter.subscribe(resetNav);
     onDestroy(unsubscribe);
+
     onMount(async () => {
         try {
             informations = await callApi("get", "/informations");
@@ -66,6 +73,7 @@
         } catch (e) {
             informations = "network";
         }
+
         setTimeout(async () => {
                 try {
 
@@ -76,6 +84,7 @@
                 }
             }, 1000
         );
+
         await calculateProperties(user);
         loaded = true;
     });
@@ -109,27 +118,27 @@
                 bugs on the website.
             </p>
             <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                    on:click={() => (offline = false)}>
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                on:click={() => (offline = false)}>
                 <path
-                        class="heroicon-ui"
-                        d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1
+                    class="heroicon-ui"
+                    d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1
                     0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12
                     10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"
-                        fill="#FFFFFF"/>
+                    fill="#FFFFFF" />
             </svg>
         </div>
     {/if}
     <nav
-            class:border-green={isScrolling || (loaded && poll)}
-            class:border-b-2={loaded && poll}
-            class="shadow-link-hover bg-background lg:flex items-center text-font
+        class:border-green={isScrolling || (loaded && poll)}
+        class:border-b-2={loaded && poll}
+        class="shadow-link-hover bg-background lg:flex items-center text-font
         w-full transition duration-200 border-b border-transparent">
         <div
-                class="w-full lg:w-auto flex justify-between items-center py-3
+            class="w-full lg:w-auto flex justify-between items-center py-3
             relative">
             <div class="pl-7 lg:pl-24 lg:pr-34 text-logo">
                 <a class="logo" href="/">WINHALLA</a>
@@ -137,40 +146,40 @@
             <div class="pr-6 lg:hidden flex -mt-2">
                 <div class="flex lg:hidden items-center">
                     {#if informations}
-                        <NavAlert data={informations}/>
+                        <NavAlert data={informations} />
                     {/if}
 
-                    <Notifications data={notificationsObj}/>
+                    <Notifications data={notificationsObj} />
                 </div>
                 <button
-                        class="focus:outline-none"
-                        use:clickOutside
-                        on:click_outside={() => (isNavbarOpen = false)}
-                        on:click={() => {
+                    class="focus:outline-none"
+                    use:clickOutside
+                    on:click_outside={() => (isNavbarOpen = false)}
+                    on:click={() => {
                         isNavbarOpen = !isNavbarOpen;
                     }}>
                     <svg
-                            class="w-7 h-7 fill-current nav-icon"
-                            viewBox="0 0 28 24"
-                            xmlns="http://www.w3.org/2000/svg">
+                        class="w-7 h-7 fill-current nav-icon"
+                        viewBox="0 0 28 24"
+                        xmlns="http://www.w3.org/2000/svg">
                         {#if !isNavbarOpen}
                             <path
-                                    d="m2.61 0h22.431c1.441 0 2.61 1.168 2.61
+                                d="m2.61 0h22.431c1.441 0 2.61 1.168 2.61
                                 2.61s-1.168 2.61-2.61 2.61h-22.431c-1.441
-                                0-2.61-1.168-2.61-2.61s1.168-2.61 2.61-2.61z"/>
+                                0-2.61-1.168-2.61-2.61s1.168-2.61 2.61-2.61z" />
                             <path
-                                    d="m2.61 9.39h22.431c1.441 0 2.61 1.168 2.61
+                                d="m2.61 9.39h22.431c1.441 0 2.61 1.168 2.61
                                 2.61s-1.168 2.61-2.61 2.61h-22.431c-1.441
-                                0-2.61-1.168-2.61-2.61s1.168-2.61 2.61-2.61z"/>
+                                0-2.61-1.168-2.61-2.61s1.168-2.61 2.61-2.61z" />
                             <path
-                                    d="m2.61 18.781h22.431c1.441 0 2.61 1.168 2.61
+                                d="m2.61 18.781h22.431c1.441 0 2.61 1.168 2.61
                                 2.61s-1.168 2.61-2.61 2.61h-22.431c-1.441
-                                0-2.61-1.168-2.61-2.61s1.168-2.61 2.61-2.61z"/>
+                                0-2.61-1.168-2.61-2.61s1.168-2.61 2.61-2.61z" />
                         {:else}
                             <path
-                                    d="m24 2.4-2.4-2.4-9.6 9.6-9.6-9.6-2.4 2.4 9.6
+                                d="m24 2.4-2.4-2.4-9.6 9.6-9.6-9.6-2.4 2.4 9.6
                                 9.6-9.6 9.6 2.4 2.4 9.6-9.6 9.6 9.6
-                                2.4-2.4-9.6-9.6z"/>
+                                2.4-2.4-9.6-9.6z" />
                         {/if}
                     </svg>
                 </button>
@@ -178,7 +187,7 @@
         </div>
         <div class:hidden={!isNavbarOpen} class="lg:block w-full">
             <div
-                    class="pb-3 lg:p-0 sm:flex items-center w-full justify-between">
+                class="pb-3 lg:p-0 sm:flex items-center w-full justify-between">
                 <div class="ml-7 links text-xl lg:flex">
                     <!--<a
                             class="nav-link-container lg:hover:text-shadow-link-hover
@@ -194,35 +203,35 @@
                         PROFILE
                     </a>-->
                     <a
-                            class="nav-link-container
+                        class="nav-link-container
                         lg:hover:text-shadow-link-hover border-l border-primary
                         lg:border-none pl-3"
-                            href="/play"
-                            rel="prefetch">
+                        href="/play"
+                        rel="prefetch">
                         <svg
-                                class="fill-current play"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg">
+                            class="fill-current play"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
-                                    d="m.001 1.165v21.669c.052.661.601 1.177 1.271
+                                d="m.001 1.165v21.669c.052.661.601 1.177 1.271
                                 1.177.225 0 .436-.058.62-.16l-.006.003
                                 21.442-10.8c.4-.192.671-.593.671-1.058s-.271-.867-.664-1.055l-.007-.003-21.442-10.8c-.177-.099-.388-.157-.613-.157-.672
-                                0-1.223.521-1.27 1.181v.004z"/>
+                                0-1.223.521-1.27 1.181v.004z" />
                         </svg>
                         PLAY
                     </a>
                     <a
-                            class="nav-link-container
+                        class="nav-link-container
                         lg:hover:text-shadow-link-hover border-l border-primary
                         lg:border-none pl-3"
-                            href="/shop"
-                            rel="prefetch">
+                        href="/shop"
+                        rel="prefetch">
                         <svg
-                                class="fill-current play"
-                                viewBox="0 0 22 24"
-                                xmlns="http://www.w3.org/2000/svg">
+                            class="fill-current play"
+                            viewBox="0 0 22 24"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
-                                    d="m14.416 24v-11.098h5.68c.181 0
+                                d="m14.416 24v-11.098h5.68c.181 0
                                 .328.147.328.328v10.114c0
                                 .362-.294.656-.656.656zm-12.096 0c-.362
                                 0-.656-.294-.656-.656v-10.114c0-.181.147-.328.328-.328h5.621v11.098zm-1.992-12.08c-.181
@@ -249,7 +258,7 @@
                                 2.596-.871-2.955-2.053-4.342-2.65-4.342-.329.056-.609.229-.804.473zm5.315
                                 3.791c1.692-.501 3.698-1.389
                                 4.043-2.406.048-.142.194-.572-.422-1.291-.183-.271-.469-.461-.801-.513l-.007-.001c-.946
-                                0-2.103 2.226-2.813 4.21z"/>
+                                0-2.103 2.226-2.813 4.21z" />
                         </svg>
                         SHOP
                     </a>
@@ -308,19 +317,19 @@
                 <div class="ml-7 mt-5 md:m-0 md:mr-7 lg:flex lg:items-center">
                     {#if informations}
                         <div class="hidden lg:flex items-center">
-                            <NavAlert data={informations}/>
+                            <NavAlert data={informations} />
                         </div>
                     {/if}
                     {#if isUserLoggedIn === true}
                         <div class="lg:flex lg:items-center">
                             {#if user.displayName && user.photos}
                                 <NavAccount
-                                        username={user.displayName}
-                                        avatar={user.photos[0].value}/>
+                                    username={user.displayName}
+                                    avatar={user.photos[0].value} />
                             {/if}
                             {#if notificationsObj}
                                 <div class="hidden lg:flex items-center">
-                                    <Notifications data={notificationsObj}/>
+                                    <Notifications data={notificationsObj} />
                                 </div>
                             {/if}
 
@@ -331,21 +340,21 @@
                         </div>
                     {:else if isUserLoggedIn == 'steam'}
                         <a
-                                class="button-brand button mr-3"
-                                href="/create-account">
+                            class="button-brand button mr-3"
+                            href="/create-account">
                             CREATE ACCOUNT
                         </a>
                     {:else if isUserLoggedIn === 'network'}
                         <p class="text-legendary text-xl">An error occured processing the account data</p>
                     {:else}
                         <a
-                                class="button-brand button mr-3"
-                                href="{apiUrl}/auth/login">
+                            class="button-brand button mr-3"
+                            href="{apiUrl}/auth/login">
                             CREATE ACCOUNT
                         </a>
                         <a
-                                class="button-brand-alternative button"
-                                href="{apiUrl}/auth/login">
+                            class="button-brand-alternative button"
+                            href="{apiUrl}/auth/login">
                             LOGIN
                         </a>
                     {/if}
@@ -355,8 +364,11 @@
         </div>
 
     </nav>
-    <div class="fixed z-10 left-1/2 w-full md:left-auto md:right-8 top-19 text-font text-default max-w-sm transform -translate-x-1/2 md:translate-x-0 px-5 md:px-0">
-        <Poll poll={poll}/>
-    </div>
-
+    {#if loaded}
+        <div
+            class="fixed z-10 left-1/2 w-full md:left-auto md:right-8 top-19 text-font text-default max-w-sm transform -translate-x-1/2 md:translate-x-0 px-5 md:px-0"
+            transition:fly={{ y:-200, duration: 500 }}>
+            <Poll poll={poll} />
+        </div>
+    {/if}
 </div>
--- src/components/Poll.svelte
+++ src/components/Poll.svelte
@@ -1,104 +1,103 @@
 <script>
     import { callApi } from "../utils/api";
+    import { fly } from "svelte/transition";
 
-    export let poll = {
+    export let poll;/* = {
         name: "Should we add a 2vs2 game mode ?",
         isMCQ: true,
         options: [
             "Yes", "I don't care", "No"
         ]
-    };
+    };*/
 
 
     let answer;
     let answered = false;
-    let isPollOpen = true;
+
+    let isPollOpen = false;
 
     function handleClick() {
-
         isPollOpen = !isPollOpen;
-
     }
 
     function handleChoose(number) {
-        console.log("test");
-        /*if(answer === number) {
-            return answer = undefined;
-        }*/
-
         answer = number;
-        console.log(answer);
     }
-
-    setInterval(() => {
-        if (poll.submitted === true) handleSubmit();
-        poll.submitted = false;
-    }, 500);
 
     async function handleSubmit() {
         answered = true;
         await callApi("post", `/pollresponse?answer=${answer}&name=${poll.name}`);
+        setTimeout(() => {
+            poll = undefined;
+        }, 2000);
     }
 </script>
 
 
-<div class:pb-4={isPollOpen} class="bg-background rounded-lg border border-primary">
-    <button class="flex justify-between cursor-pointer focus:outline-none w-full" on:click={() => handleClick()}>
-        <p class:pb-4={!isPollOpen} class="pl-5 pt-5">{poll.name}</p>
-        <!--Svg icon-->
-        <div class="ml-5 mr-3 mt-6">
-            <svg class:hidden={isPollOpen} class="fill-current w-5" style="margin-bottom: 0.14rem;" viewBox="0 0 24 24"
-                 xmlns="http://www.w3.org/2000/svg">
-                <path d="m21.57 19.2 2.43-2.422-12-11.978-12 11.978 2.43 2.422 9.57-9.547z" />
-            </svg>
-            <svg class:hidden={!isPollOpen} class="fill-current w-5" style="margin-bottom: 0.14rem;" viewBox="0 0 24 24"
-                 xmlns="http://www.w3.org/2000/svg">
-                <path d="m2.43 4.8-2.43 2.422 12 11.978 12-11.978-2.43-2.422-9.57 9.547z" />
-            </svg>
-        </div>
-
-    </button>
-
-    <div class:hidden={!isPollOpen} class="px-5">
-        {#if poll.isMCQ}
-            <div class="">
-                {#if answered === false}
-                    {#each poll.options as option, i}
-                        <div on:click={()=>{handleChoose(i)}}
-                             class:border-primary={answer === i}
-                             class="p-4 bg-variant my-3 rounded-lg border-2 border-transparent hover:border-primary focus:outline-none focus:border-primary block flex items-center cursor-pointer">
-
-                            <input class="opacity-0 fixed pointer-events-none w-4 h-4" type="radio" value={option}>
-
-                            {#if answer === i}
-                                <svg class="w-4 fill-current text-primary" viewBox="0 0 24 24"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="m0 12c0-6.627 5.373-12 12-12s12 5.373 12 12-5.373 12-12 12c-6.624-.008-11.992-5.376-12-11.999zm2.4 0c0 5.302 4.298 9.6 9.6 9.6s9.6-4.298 9.6-9.6-4.298-9.6-9.6-9.6c-5.299.006-9.594 4.301-9.6 9.599v.001zm4 0c0-3.093 2.507-5.6 5.6-5.6s5.6 2.507 5.6 5.6-2.507 5.6-5.6 5.6c-3.093 0-5.6-2.507-5.6-5.6z" />
-                                </svg>
-                            {:else}
-                                <svg class="w-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="m12 24c-6.627 0-12-5.373-12-12s5.373-12 12-12 12 5.373 12 12c-.008 6.624-5.376 11.992-11.999 12zm0-21.6c-5.302 0-9.6 4.298-9.6 9.6s4.298 9.6 9.6 9.6 9.6-4.298 9.6-9.6c-.006-5.299-4.301-9.594-9.599-9.6h-.001z" />
-                                </svg>
-                            {/if}
-
-
-                            <p class="ml-2" style="line-height: 0; margin-bottom: -0.20rem">{option}</p>
-
-                        </div>
-
-                    {/each}
-                    <button on:submit={handleSubmit()} class="button button-brand w-24 mt-2 w-full"
-                            style="">
-                        SUBMIT
-                    </button>
-                {:else}
-                    <p class="text-3xl text-center mx-auto rounded-lg focus:outline-none block text-primary mt-2">Thanks
-                        for your
-                        answer!</p>
-                {/if}
+{#if poll}
+    <div class:pb-4={isPollOpen} class="bg-background rounded-lg border border-primary"
+         transition:fly={{ x:200, duration: 300 }}>
+        <button class="flex justify-between cursor-pointer focus:outline-none w-full" on:click={() => handleClick()}>
+            <p class:pb-4={!isPollOpen} class="pl-5 pt-5">{poll.name}</p>
+            <!--Svg icon-->
+            <div class="ml-5 mr-3 mt-6">
+                <svg class:hidden={isPollOpen} class="fill-current w-5" style="margin-bottom: 0.14rem;"
+                     viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path d="m21.57 19.2 2.43-2.422-12-11.978-12 11.978 2.43 2.422 9.57-9.547z" />
+                </svg>
+                <svg class:hidden={!isPollOpen} class="fill-current w-5" style="margin-bottom: 0.14rem;"
+                     viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path d="m2.43 4.8-2.43 2.422 12 11.978 12-11.978-2.43-2.422-9.57 9.547z" />
+                </svg>
             </div>
-        {/if}
+
+        </button>
+
+        <div class:hidden={!isPollOpen} class="px-5">
+            {#if poll.isMCQ}
+                <div class="">
+                    {#if answered === false}
+                        {#each poll.options as option, i}
+                            <div on:click={()=>{handleChoose(i)}}
+                                 class:border-primary={answer === i}
+                                 class="p-4 bg-variant my-3 rounded-lg border-2 border-transparent hover:border-primary focus:outline-none focus:border-primary block flex items-center cursor-pointer">
+
+                                <input class="opacity-0 fixed pointer-events-none w-4 h-4" type="radio" value={option}>
+
+                                {#if answer === i}
+                                    <svg class="w-4 fill-current text-primary" viewBox="0 0 24 24"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="m0 12c0-6.627 5.373-12 12-12s12 5.373 12 12-5.373 12-12 12c-6.624-.008-11.992-5.376-12-11.999zm2.4 0c0 5.302 4.298 9.6 9.6 9.6s9.6-4.298 9.6-9.6-4.298-9.6-9.6-9.6c-5.299.006-9.594 4.301-9.6 9.599v.001zm4 0c0-3.093 2.507-5.6 5.6-5.6s5.6 2.507 5.6 5.6-2.507 5.6-5.6 5.6c-3.093 0-5.6-2.507-5.6-5.6z" />
+                                    </svg>
+                                {:else}
+                                    <svg class="w-4 fill-current" viewBox="0 0 24 24"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="m12 24c-6.627 0-12-5.373-12-12s5.373-12 12-12 12 5.373 12 12c-.008 6.624-5.376 11.992-11.999 12zm0-21.6c-5.302 0-9.6 4.298-9.6 9.6s4.298 9.6 9.6 9.6 9.6-4.298 9.6-9.6c-.006-5.299-4.301-9.594-9.599-9.6h-.001z" />
+                                    </svg>
+                                {/if}
+
+
+                                <p class="ml-2" style="line-height: 0; margin-bottom: -0.20rem">{option}</p>
+
+                            </div>
+
+                        {/each}
+                        <button on:click={()=> handleSubmit()} class="button button-brand w-24 mt-2 w-full">
+                            SUBMIT
+                        </button>
+                    {:else}
+                        <p class="text-3xl text-center mx-auto rounded-lg focus:outline-none block text-primary mt-2">
+                            Thanks
+                            for your
+                            answer!</p>
+                    {/if}
+                </div>
+            {/if}
+        </div>
     </div>
-</div>
+
+{/if}

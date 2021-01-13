2aebb8612458bc94c6706aa6ae6e153a4ab6b29f
23felons23 <slavikzhyhota@gmail.com>
2021-01-13 22:28:18 +0100

    Only show the GuideCard when the user still didn't visited the page

---
+++ src/utils/getCookie.js
@@ -0,0 +1,15 @@
+export function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
+
+    for (let c of ca) {
+
+        while (c.charAt(0) === " ") {
+            c = c.substring(1, c.length);
+        }
+
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+

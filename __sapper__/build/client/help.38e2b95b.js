4706d0be26d61739866703d52e16e65f590f7455
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-01-16 20:19:53 +0100

    build

--- __sapper__/build/client/help.38e2b95b.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as e,q as a,d as o,n as c}from"./client.553977fc.js";function i(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-chgju"]',document.head).forEach(o),this.h()},h(){document.title="How it works | Winhalla"},m:c,p:c,i:c,o:c,d:c}}export default class extends t{constructor(t){super(),s(this,t,null,i,e,{})}}

40ae06ae720091a868307f07ce781c8b585d1ebf
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-01-18 12:44:55 +0100

    build

--- __sapper__/build/client/terms.b47d6bfb.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as e,q as a,d as i,n as l}from"./client.b0b13b44.js";function n(s){return{c(){this.h()},l(s){a('[data-svelte="svelte-pwic88"]',document.head).forEach(i),this.h()},h(){document.title="Terms of use | Winhalla"},m:l,p:l,i:l,o:l,d:l}}export default class extends s{constructor(s){super(),t(this,s,null,n,e,{})}}

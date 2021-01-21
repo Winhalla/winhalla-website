18dc28930ec14d540af11af073e7681f05997aa6
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-01-22 00:00:31 +0100

    Update index.svelte

--- src/routes/feltrom/admin/index.svelte
+++ src/routes/feltrom/admin/index.svelte
@@ -9,29 +9,51 @@
     let isLoggedIn = false;
     let otp = "";
     let pwd = "";
-    let users;
+    let users = new Promise(() => {
+    });
     let newConfig;
-    let goldEvent = ["","","",""]
+    let goldEvent = ["", "", "", ""];
+    let loadingUsers;
 
     async function login() {
         isLoggedIn = true;
         configs = await callApi("get", `/feltrom/config?otp=${otp}&pwd=${pwd}`);
         newConfig = configs;
         users = await callApi("get", `/feltrom/users?otp=${otp}&pwd=${pwd}`);
-        goldEvent[0] = Math.floor((config.value.expiration - Date.now())/1000/86400)
-        goldEvent[1] = Math.floor((config.value.expiration - Date.now())/1000/3600 - goldEvent[0]*24)
+        goldEvent[0] = Math.floor((configs[4].value.expiration - Date.now()) / 1000 / 86400);
+        goldEvent[1] = Math.floor((configs[4].value.expiration - Date.now()) / 1000 / 3600 - goldEvent[0] * 24);
+        goldEvent[2] = Math.floor((configs[4].value.expiration - Date.now()) / 1000 / 60 - goldEvent[0] * 24 * 60 - goldEvent[1] * 60);
+    }
+
+    function loadUsers() {
+        users = callApi("get", `/feltrom/users?otp=${otp}&pwd=${pwd}`);
+        loadingUsers = true;
+        users.then(result => {
+            users = result;
+            users.forEach((user, i) => {
+                users[i].winrate = Math.round((user.stats.ffa.wins / user.stats.ffa.gamesPlayed) * 100);
+                if ((users[i].winrate > 40/* && user.stats.gamesPlayed > 10*/) || user.coins > 300000) {
+                    user.isSuspiciousByClient = true;
+                }
+            });
+            users.sort((a, b) => {
+                return b.winrate - a.winrate;
+            });
+            loadingUsers = false;
+        });
     }
 
     onMount(async () => {
         isAuthorizedUser = (await callApi("get", "/feltrom/login")) === true;
-
         isLoggedIn = true;
         configs = await callApi("get", `/feltrom/config?otp=${otp}&pwd=${pwd}`);
         newConfig = configs;
-        users = await callApi("get", `/feltrom/users?otp=${otp}&pwd=${pwd}`);
-        goldEvent[0] = Math.floor((configs[4].value.expiration - Date.now())/1000/86400)
-        goldEvent[1] = Math.floor((configs[4].value.expiration - Date.now())/1000/3600 - goldEvent[0]*24)
-        goldEvent[2] = Math.floor((configs[4].value.expiration - Date.now())/1000/60 - goldEvent[0]*24*60 - goldEvent[1]*60)
+
+
+        goldEvent[0] = Math.floor((configs[4].value.expiration - Date.now()) / 1000 / 86400);
+        goldEvent[1] = Math.floor((configs[4].value.expiration - Date.now()) / 1000 / 3600 - goldEvent[0] * 24);
+        goldEvent[2] = Math.floor((configs[4].value.expiration - Date.now()) / 1000 / 60 - goldEvent[0] * 24 * 60 - goldEvent[1] * 60);
+
     });
 
 </script>
@@ -144,7 +166,7 @@
         <div class="lg:block lg:pl-24 lg:pr-24 mt-7 lg:mt-12 h-full w-full">
             <h1 class="text-6xl mb-12">ADMIN DASHBOARD</h1>
             <div class="flex justify-center">
-                <div class="w-1/2 mx-4 block h-full p-8">
+                <div class="w-1/3 mx-4 block h-full p-8">
                     {#each newConfig as config,i}
                         <div class="mb-16">
                             <h1 class="text-5xl text-primary">{config.name}</h1>
@@ -204,24 +226,119 @@
 
                                     {/each}
                                 {:else if config.name === "GOLD EVENT"}
-                                    <h3 class="text-3xl">Boost of <strong class="font-normal text-accent">{config.value.percentage - 100}%</strong></h3>
-                                    <p class="text-3xl">
+                                    <h3 class="text-2xl">Boost of <strong
+                                        class="font-normal text-accent text-3xl">{config.value.percentage - 100}
+                                        %</strong></h3>
+                                    <p class="text-2xl">
                                         Exipires in
-                                        <strong class="text-accent font-normal">{goldEvent[0]}</strong> days,
-                                        <strong class="text-accent font-normal">{goldEvent[1]}</strong> hours,
-                                        <strong class="text-accent font-normal">{goldEvent[2]}</strong> minutes,
+                                        <strong class="text-accent font-normal text-3xl">{goldEvent[0]}</strong> days,
+                                        <strong class="text-accent font-normal text-3xl">{goldEvent[1]}</strong> hours,
+                                        <strong class="text-accent font-normal text-3xl">{goldEvent[2]}</strong>
+                                        minutes,
+                                    </p>
+                                {:else if config.name === "LINKS CONFIG"}
+                                    <p class="text-2xl">Players joining via an affiliated link get
+                                        <strong class="text-accent font-normal text-3xl">{config.value.boost}%</strong>
+                                        more
+                                        coins for <strong
+                                            class="text-accent font-normal text-3xl">{config.value.duration}
+                                            days</strong></p>
+                                {:else if config.name === "IDs BANNED"}
+                                    <div class="block">
+                                        {#each config.value as banned,ii}
+                                            <p>{banned}</p>
+                                        {/each}
+                                    </div>
+                                    <p class="text-3xl text-green">
+                                        {config.value.length === 0 ? "No player has been banned" : ""}
                                     </p>
                                 {/if}
                             </div>
                         </div>
                     {/each}
                 </div>
-                <div class="w-1/4 h-full block">
-                    <!--{#each users as user,i}
-                        {user.brawlhallaName}
-                    {/each}-->
+                <div class="w-2/3 h-full block">
+                    {#await users}
+                        {#if !loadingUsers}
+                            <button class="button button-brand" on:click={loadUsers}>Load users (can take long and up to
+                                50 MO of data)
+                            </button>
+                        {:else}
+                            Loading user data...
+                        {/if}
+                    {:then users}
+                        <div class="px-4 text-2xl overflow-hidden mt-10 lg:mt-20">
+                            <h3>Sort by: <strong class="font-normal">winrate</strong>, <strong
+                                class="font-normal">coins</strong>, <strong class="font-normal">wins</strong>, <strong
+                                class="font-normal">games played</strong></h3>
+                        </div>
+                        <table class="card px-4 text-2xl overflow-hidden mt-10 lg:mt-20">
+                            <thead class="bg-primary ">
+                            <tr>
+                                <td class="px-6 py-3">
+
+                                </td>
+                                <td class="px-6 py-3">
+                                    Name
+                                </td>
+                                <td class="px-6 py-3">
+                                    In game ?
+                                </td>
+                                <td class="px-6 py-3">
+                                    Wins
+                                </td>
+                                <td class="px-6 py-3">
+                                    Losses
+                                </td>
+                                <td class="px-6 py-3">
+                                    Winrate
+                                </td>
+                                <td class="px-6 py-3">
+                                    Coins
+                                </td>
+                            </tr>
+                            </thead>
+                            <tbody class="  text-l">
+                            <!--For each rank-->
+                            {#each users as user,i}
+                                <tr class="border border-background text-center"
+                                    class:border-legendary={user.isSuspiciousByClient || user.isSucpicious.ffa ||user.isSucpicious.solo}
+                                >
+                                    <td class="px-6 py-2">
+                                        <b class="font-normal">{i + 1}</b>
+                                    </td>
+                                    <td class="flex items-center px-6 py-2">
+                                        <img class="block w-10 h-10 rounded-full" src={user.avatarURL}
+                                             alt={user.brawlhallaName}>
+                                        <p class="pl-2">{user.brawlhallaName}</p>
+                                    </td>
+                                    <td class="px-6 py-2">
+                                        <b class:text-legendary={user.inGame.findIndex(e => !e.isFinished) === -1}
+                                           class:text-green={user.inGame.findIndex(e => !e.isFinished) !== -1}
+                                           class="font-normal">{user.inGame.findIndex(e => !e.isFinished) === -1 ? "No" : "Yes"}</b>
+                                    </td>
+                                    <td class="px-6 py-2">
+                                        {user.stats.ffa.wins}
+                                    </td>
+                                    <td class="px-6 py-2">
+                                        {user.stats.ffa.gamesPlayed}
+                                    </td>
+                                    <td class:text-green={user.winrate<14}
+                                        class:text-accent={user.winrate<25 && user.winrate>=14}
+                                        class:text-legendary={user.winrate>=25} class="px-6 py-2">
+                                        {user.winrate}%
+                                    </td>
+                                    <td class:text-green={user.coins<40000}
+                                        class:text-accent={user.coins<120000 && user.coins>=40000}
+                                        class:text-legendary={user.coins>=120000} class="px-6 py-2">
+                                        {user.coins}$
+                                    </td>
+                                </tr>
+                            {/each}
+                            </tbody>
+                        </table>
+                    {/await}
                 </div>
-                <div class="w-1/4 mx-4 h-full">c</div>
             </div>
         </div>
     {/if}

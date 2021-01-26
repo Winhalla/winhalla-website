5af57c90ab18b82428f5a70c20bb40123a70d8d6
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-01-26 20:12:50 +0100

    fixed bug on unsuspicious

--- src/components/UsersArray.svelte
+++ src/components/UsersArray.svelte
@@ -16,6 +16,20 @@
     let action;
     let reason;
 
+    async function unsuspicious(id, state) {
+        if (state === 0) {
+            isDoingAction = true;
+            player = id;
+            action = "Mark as unsuspicious";
+        }
+        if (state === 1) {
+            await callApi("post", `/feltrom/unsuspicious?otp=${otp}&pwd=${pwd}&user=${id}`);
+            config.set({ refresh: true });
+            isDoingAction = false;
+            player = undefined;
+        }
+    }
+
     async function ban(id, state, ban) {
         if (state === 0) {
             isDoingAction = true;
@@ -73,13 +87,13 @@
             <td class="px-2 py-2">
                 <b class="font-normal">{i + 1}</b>
             </td>
-            <td class="h-full px-2 py-2" >
+            <td class="h-full px-2 py-2">
                 <div class="flex my-auto overflow-x-auto" style="width: 13rem">
-                <img class="w-10 h-10 rounded-full" src={user.avatarURL}
-                     alt="PP">
-                <p class="pl-2" class:text-lg={user.brawlhallaName.length>=17 && user.brawlhallaName.length<26}>
-                    {user.brawlhallaName}
-                </p>
+                    <img class="w-10 h-10 rounded-full" src={user.avatarURL}
+                         alt="PP">
+                    <p class="pl-2" class:text-lg={user.brawlhallaName.length>=17 && user.brawlhallaName.length<26}>
+                        {user.brawlhallaName}
+                    </p>
                 </div>
             </td>
             {#if !banned}
@@ -109,13 +123,24 @@
                 </td>
             {/if}
             {#if !banned}
-                <td class="px-2 py-2">
-                    <div on:click={ban(user.steamId,0,true)} class="cursor-pointer">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
-                             style="fill: #fc1870;">
-                            <path
-                                d="m20.46 11.992c0-.018 0-.038 0-.059 0-1.69-.507-3.261-1.376-4.571l.019.031-11.757 11.74c1.296.87 2.89 1.388 4.606 1.388h.026-.001.029c1.182 0 2.306-.25 3.321-.699l-.052.021c3.074-1.315 5.188-4.314 5.188-7.807 0-.015 0-.03 0-.045v.002zm-15.576 4.662 11.773-11.757c-1.29-.889-2.886-1.42-4.607-1.42-.025 0-.05 0-.074 0h.004c-.019 0-.041 0-.064 0-1.546 0-2.992.423-4.231 1.159l.038-.021c-2.544 1.51-4.223 4.244-4.223 7.369 0 1.736.518 3.352 1.408 4.7l-.02-.032zm19.066-4.662v.035c0 1.678-.35 3.273-.981 4.718l.03-.076c-1.842 4.36-6.082 7.363-11.024 7.363s-9.182-3.004-10.994-7.285l-.029-.078c-.601-1.379-.951-2.985-.951-4.674s.35-3.295.981-4.751l-.03.077c1.842-4.36 6.082-7.363 11.024-7.363s9.182 3.004 10.994 7.285l.029.078c.601 1.365.952 2.957.952 4.631v.041-.002z" />
-                        </svg>
+                <td class="px-2 py-2 block w-15">
+                    <div class="block">
+                        <div on:click={ban(user.steamId,0,true)} class="cursor-pointer">
+                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
+                                 style="fill: #fc1870;">
+                                <path
+                                    d="m20.46 11.992c0-.018 0-.038 0-.059 0-1.69-.507-3.261-1.376-4.571l.019.031-11.757 11.74c1.296.87 2.89 1.388 4.606 1.388h.026-.001.029c1.182 0 2.306-.25 3.321-.699l-.052.021c3.074-1.315 5.188-4.314 5.188-7.807 0-.015 0-.03 0-.045v.002zm-15.576 4.662 11.773-11.757c-1.29-.889-2.886-1.42-4.607-1.42-.025 0-.05 0-.074 0h.004c-.019 0-.041 0-.064 0-1.546 0-2.992.423-4.231 1.159l.038-.021c-2.544 1.51-4.223 4.244-4.223 7.369 0 1.736.518 3.352 1.408 4.7l-.02-.032zm19.066-4.662v.035c0 1.678-.35 3.273-.981 4.718l.03-.076c-1.842 4.36-6.082 7.363-11.024 7.363s-9.182-3.004-10.994-7.285l-.029-.078c-.601-1.379-.951-2.985-.951-4.674s.35-3.295.981-4.751l-.03.077c1.842-4.36 6.082-7.363 11.024-7.363s9.182 3.004 10.994 7.285l.029.078c.601 1.365.952 2.957.952 4.631v.041-.002z" />
+                            </svg>
+                        </div>
+                        {#if color === "red"}
+                            <div on:click={unsuspicious(user.steamId,0)} class="cursor-pointer">
+                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
+                                     style="fill: #3de488;">
+                                    <path
+                                        d="m20.46 11.992c0-.018 0-.038 0-.059 0-1.69-.507-3.261-1.376-4.571l.019.031-11.757 11.74c1.296.87 2.89 1.388 4.606 1.388h.026-.001.029c1.182 0 2.306-.25 3.321-.699l-.052.021c3.074-1.315 5.188-4.314 5.188-7.807 0-.015 0-.03 0-.045v.002zm-15.576 4.662 11.773-11.757c-1.29-.889-2.886-1.42-4.607-1.42-.025 0-.05 0-.074 0h.004c-.019 0-.041 0-.064 0-1.546 0-2.992.423-4.231 1.159l.038-.021c-2.544 1.51-4.223 4.244-4.223 7.369 0 1.736.518 3.352 1.408 4.7l-.02-.032zm19.066-4.662v.035c0 1.678-.35 3.273-.981 4.718l.03-.076c-1.842 4.36-6.082 7.363-11.024 7.363s-9.182-3.004-10.994-7.285l-.029-.078c-.601-1.379-.951-2.985-.951-4.674s.35-3.295.981-4.751l-.03.077c1.842-4.36 6.082-7.363 11.024-7.363s9.182 3.004 10.994 7.285l.029.078c.601 1.365.952 2.957.952 4.631v.041-.002z" />
+                                </svg>
+                            </div>
+                        {/if}
                     </div>
                 </td>
             {:else}
@@ -162,7 +187,7 @@
         {/if}
         <div class="justify-center w-full flex">
             <button class="button button-brand mt-8" style="background-color: #fc1870"
-                    on:click={()=>ban(player,1,action)}>
+                    on:click={()=>{if(action === "Mark as unsuspicious"){unsuspicious(player,1)}else{ban(player,1,action)}}}>
                 Confirm {action}
             </button>
             <button class="button button-brand mt-8 border border-legendary ml-5" style="background-color: #17171a;"
--- src/routes/feltrom/admin/index.svelte
+++ src/routes/feltrom/admin/index.svelte
@@ -102,7 +102,15 @@
                 let { avatarURL, brawlhallaName, stats, coins } = users.find(user => user.steamId === ban.id);
                 let winrate = Math.round((stats.ffa.wins / stats.ffa.gamesPlayed) * 100);
                 if (isNaN(winrate)) winrate = 0;
-                bannedOnes[i] = { avatarURL, steamId: ban.id, brawlhallaName, stats, coins, winrate, reason:ban.reason };
+                bannedOnes[i] = {
+                    avatarURL,
+                    steamId: ban.id,
+                    brawlhallaName,
+                    stats,
+                    coins,
+                    winrate,
+                    reason: ban.reason
+                };
                 users.splice(users.findIndex(e => e.steamId === ban), 1);
 
                 loadingUsers = false;
@@ -319,24 +327,27 @@
                 </div>
                 <div class="w-2/3 h-full block">
                     {#if users}
-                        <div class="px-4 text-2xl mb-5 flex overflow-hidden">
-                            <h3 class="">Sort by:</h3>
-                            <h3 class="ml-3 -mt-2px">
-                                <strong class="font-normal cursor-pointer " class:text-primary={sortBy === "alphabetic"}
-                                        class:text-3xl={sortBy === "alphabetic"}
-                                        on:click={()=>sort("alphabetic")}>alphabetic</strong>,
-                                <strong class="font-normal cursor-pointer" class:text-primary={sortBy === "winrate"}
-                                        class:text-3xl={sortBy === "winrate"}
-                                        on:click={()=>sort("winrate")}>winrate</strong>,
-                                <strong class="font-normal cursor-pointer" class:text-primary={sortBy === "coins"}
-                                        class:text-3xl={sortBy === "coins"}
-                                        on:click={()=>sort("coins")}>coins</strong>,
-                                <strong class="font-normal cursor-pointer" class:text-primary={sortBy === "gamesPlayed"}
-                                        class:text-3xl={sortBy === "gamesPlayed"}
-                                        on:click={()=>sort("gamesPlayed",true)}>games played</strong>
-                            </h3>
-
-                        </div>
+                        {#if normalUsersShown || suspiciousBitches.length > 0}
+                            <div class="px-4 text-2xl mb-5 flex overflow-hidden">
+                                <h3 class="">Sort by:</h3>
+                                <h3 class="ml-3 -mt-2px">
+                                    <strong class="font-normal cursor-pointer "
+                                            class:text-primary={sortBy === "alphabetic"}
+                                            class:text-3xl={sortBy === "alphabetic"}
+                                            on:click={()=>sort("alphabetic")}>alphabetic</strong>,
+                                    <strong class="font-normal cursor-pointer" class:text-primary={sortBy === "winrate"}
+                                            class:text-3xl={sortBy === "winrate"}
+                                            on:click={()=>sort("winrate")}>winrate</strong>,
+                                    <strong class="font-normal cursor-pointer" class:text-primary={sortBy === "coins"}
+                                            class:text-3xl={sortBy === "coins"}
+                                            on:click={()=>sort("coins")}>coins</strong>,
+                                    <strong class="font-normal cursor-pointer"
+                                            class:text-primary={sortBy === "gamesPlayed"}
+                                            class:text-3xl={sortBy === "gamesPlayed"}
+                                            on:click={()=>sort("gamesPlayed",true)}>games played</strong>
+                                </h3>
+                            </div>
+                        {/if}
                         <div class="flex">
                             <div class="block">
                                 {#if suspiciousBitches.length > 0}
@@ -348,30 +359,41 @@
                                         <UsersArray users="{suspiciousBitches}" color="red" pwd="{pwd}" />
                                     </div>
                                 {:else}
-                                    <div class="my-5 mb-5">
+                                    <div class="my-5 text-3xl mb-5 text-green">
                                         No suspicious player has been found
                                     </div>
                                 {/if}
                                 {#if normalUsersShown}
                                     <UsersArray users="{users}" color="blue" pwd="{pwd}" />
-                                    <h2 class="text-2xl hover:underline ml-3 mt-4 text-gray-300 hover:text-white"
-                                        on:click={()=>normalUsersShown = !normalUsersShown}>Click to hide users</h2>
-                                {:else}
-                                    <h2 class="text-2xl hover:underline ml-3 mt-4 text-gray-300 hover:text-white"
-                                        on:click={()=>normalUsersShown = !normalUsersShown}>Click to display all
+                                    <h2 class="text-2xl cursor-pointer ml-3 mt-2 w-22 text-gray-400 hover:text-white"
+                                        on:click={()=>normalUsersShown = !normalUsersShown}>hide
                                         users</h2>
+                                {:else}
+                                    <button class="button button-brand ml-3 mt-4"
+                                            on:click={()=>normalUsersShown = !normalUsersShown}>Show all users
+                                    </button>
                                 {/if}
                             </div>
-                            <div class="block ml-5 mt-11 mr-10%">
-                                <p class="mt-6 text-xl">
-                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mb-2"
-                                         style="fill: #fc1870">
-                                        <path
-                                            d="m20.46 11.992c0-.018 0-.038 0-.059 0-1.69-.507-3.261-1.376-4.571l.019.031-11.757 11.74c1.296.87 2.89 1.388 4.606 1.388h.026-.001.029c1.182 0 2.306-.25 3.321-.699l-.052.021c3.074-1.315 5.188-4.314 5.188-7.807 0-.015 0-.03 0-.045v.002zm-15.576 4.662 11.773-11.757c-1.29-.889-2.886-1.42-4.607-1.42-.025 0-.05 0-.074 0h.004c-.019 0-.041 0-.064 0-1.546 0-2.992.423-4.231 1.159l.038-.021c-2.544 1.51-4.223 4.244-4.223 7.369 0 1.736.518 3.352 1.408 4.7l-.02-.032zm19.066-4.662v.035c0 1.678-.35 3.273-.981 4.718l.03-.076c-1.842 4.36-6.082 7.363-11.024 7.363s-9.182-3.004-10.994-7.285l-.029-.078c-.601-1.379-.951-2.985-.951-4.674s.35-3.295.981-4.751l-.03.077c1.842-4.36 6.082-7.363 11.024-7.363s9.182 3.004 10.994 7.285l.029.078c.601 1.365.952 2.957.952 4.631v.041-.002z" />
-                                    </svg>
-                                    Click to ban user (confirmation message always shows)
-                                </p>
-                            </div>
+                            {#if normalUsersShown || suspiciousBitches.length > 0}
+                                <div class="block ml-5 mt-11 mr-10%">
+                                    <p class="mt-6 text-xl">
+                                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mb-2"
+                                             style="fill: #fc1870">
+                                            <path
+                                                d="m20.46 11.992c0-.018 0-.038 0-.059 0-1.69-.507-3.261-1.376-4.571l.019.031-11.757 11.74c1.296.87 2.89 1.388 4.606 1.388h.026-.001.029c1.182 0 2.306-.25 3.321-.699l-.052.021c3.074-1.315 5.188-4.314 5.188-7.807 0-.015 0-.03 0-.045v.002zm-15.576 4.662 11.773-11.757c-1.29-.889-2.886-1.42-4.607-1.42-.025 0-.05 0-.074 0h.004c-.019 0-.041 0-.064 0-1.546 0-2.992.423-4.231 1.159l.038-.021c-2.544 1.51-4.223 4.244-4.223 7.369 0 1.736.518 3.352 1.408 4.7l-.02-.032zm19.066-4.662v.035c0 1.678-.35 3.273-.981 4.718l.03-.076c-1.842 4.36-6.082 7.363-11.024 7.363s-9.182-3.004-10.994-7.285l-.029-.078c-.601-1.379-.951-2.985-.951-4.674s.35-3.295.981-4.751l-.03.077c1.842-4.36 6.082-7.363 11.024-7.363s9.182 3.004 10.994 7.285l.029.078c.601 1.365.952 2.957.952 4.631v.041-.002z" />
+                                        </svg>
+                                        Click to ban user (confirmation message always shows)
+                                    </p>
+                                    <p class="mt-6 text-xl">
+                                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mb-2"
+                                             style="fill: #3de488">
+                                            <path
+                                                d="m20.46 11.992c0-.018 0-.038 0-.059 0-1.69-.507-3.261-1.376-4.571l.019.031-11.757 11.74c1.296.87 2.89 1.388 4.606 1.388h.026-.001.029c1.182 0 2.306-.25 3.321-.699l-.052.021c3.074-1.315 5.188-4.314 5.188-7.807 0-.015 0-.03 0-.045v.002zm-15.576 4.662 11.773-11.757c-1.29-.889-2.886-1.42-4.607-1.42-.025 0-.05 0-.074 0h.004c-.019 0-.041 0-.064 0-1.546 0-2.992.423-4.231 1.159l.038-.021c-2.544 1.51-4.223 4.244-4.223 7.369 0 1.736.518 3.352 1.408 4.7l-.02-.032zm19.066-4.662v.035c0 1.678-.35 3.273-.981 4.718l.03-.076c-1.842 4.36-6.082 7.363-11.024 7.363s-9.182-3.004-10.994-7.285l-.029-.078c-.601-1.379-.951-2.985-.951-4.674s.35-3.295.981-4.751l-.03.077c1.842-4.36 6.082-7.363 11.024-7.363s9.182 3.004 10.994 7.285l.029.078c.601 1.365.952 2.957.952 4.631v.041-.002z" />
+                                        </svg>
+                                        Click to unmark user
+                                    </p>
+                                </div>
+                            {/if}
                         </div>
                     {/if}
                 </div>

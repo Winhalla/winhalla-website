d813d1435ab8ce24945fff6f71f7efd4541c9e8c
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-02-08 12:28:50 +0100

    build

---
+++ __sapper__/build/client/offline.227d405d.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e}from"./client.e81c48b2.js";export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}
---
+++ __sapper__/build/client/privacy.9c0ca037.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e,q as a,d as c,n as i}from"./client.e81c48b2.js";function l(s){return{c(){this.h()},l(s){a('[data-svelte="svelte-88sy1r"]',document.head).forEach(c),this.h()},h(){document.title="Privacy policy | Winhalla"},m:i,p:i,i:i,o:i,d:i}}export default class extends s{constructor(s){super(),t(this,s,null,l,e,{})}}

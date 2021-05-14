9c3029ec2757db64ebed3dd24743ecf2c5005d11
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-05-14 20:38:06 +0200

    copy button works + share function on mobile

--- src/routes/referral-link.svelte
+++ src/routes/referral-link.svelte
@@ -13,11 +13,13 @@
     import { counter } from "../components/store";
     import Loading from "../components/Loading.svelte";
     import { apiUrl } from "../utils/config";
+    import { fade } from "svelte/transition";
 
     export let isVisible;
     let waitingTermsAcceptations;
     let generatedLink;
     let error;
+    let toolTipOpen;
     onMount(async () => {
 
         const user = await callApi("get", "/account");
@@ -44,6 +46,23 @@
         generatedLink = `http://localhost:3000/link/${generatedLink}`;
         counter.set({ refresh: true });
     }
+
+    function copyText() {
+        let temp = document.createElement("textarea");
+        temp.value = generatedLink;
+        document.body.appendChild(temp);
+        temp.select();
+        document.execCommand("copy");
+        document.body.removeChild(temp);
+        toolTipOpen = true;
+        setTimeout(() => {
+            toolTipOpen = false
+        },3000)
+    }
+
+    function share() {
+        window.navigator.share({ url: generatedLink });
+    }
 </script>
 <!--
 <Loading data="Logging in..." />
@@ -52,8 +71,21 @@
     <title>Invite friends and earn rewards | Winhalla, Play Brawlhalla. Earn rewards.</title>
 </svelte:head>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap');
+
     b {
         @apply text-primary font-normal leading-none;
+    }
+
+    .tooltip::after {
+        content: "";
+        position: absolute;
+        top: 98%;
+        right: 20%;
+        margin-left: -6px;
+        border-width: 6px;
+        border-style: solid;
+        border-color: #3d72e4 transparent transparent transparent;
     }
 
     .accent {
@@ -154,11 +186,41 @@
                 </div>
                 <div class="lg:flex justify-center">
                     <!--<textarea bind:this={areaDom}>{valueCopy}</textarea>-->
+                    {#if generatedLink}
+                        <div
+                            class="text-background  bg-font py-4 px-4 mt-14 flex items-center rounded">
+                            <div id="link" class="flex leading-none focus:outline-none focus:border-none font-semibold"
+                                 style="font-family:'Open Sans', sans-serif"><p>{generatedLink}</p>
+                                <div class="ml-2 w-5 h-5 cursor-pointer hover:text-gray-500">
+                                    <svg viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 hidden md:block"
+                                         on:click={copyText}
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="m12.922 16.587-3.671 3.671c-.693.645-1.626 1.041-2.651 1.041-2.152 0-3.896-1.744-3.896-3.896 0-1.025.396-1.958 1.043-2.654l-.002.002 3.671-3.671c.212-.23.341-.539.341-.878 0-.717-.582-1.299-1.299-1.299-.339 0-.647.13-.879.342l.001-.001-3.671 3.671c-1.108 1.162-1.789 2.74-1.789 4.476 0 3.586 2.907 6.494 6.494 6.494 1.738 0 3.316-.683 4.482-1.795l-.003.002 3.671-3.671c.212-.23.341-.539.341-.878 0-.717-.582-1.299-1.299-1.299-.339 0-.647.13-.879.342l.001-.001z" />
+                                        <path
+                                            d="m24.007 6.489c-.002-3.585-2.908-6.491-6.494-6.491-1.793 0-3.417.727-4.592 1.902l-3.671 3.671c-.259.238-.421.579-.421.958 0 .717.582 1.299 1.299 1.299.379 0 .719-.162.957-.42l.001-.001 3.671-3.671c.693-.645 1.626-1.041 2.651-1.041 2.152 0 3.896 1.744 3.896 3.896 0 1.025-.396 1.958-1.043 2.654l.002-.002-3.671 3.671c-.259.238-.421.579-.421.958 0 .717.582 1.299 1.299 1.299.379 0 .719-.162.957-.42l.001-.001 3.671-3.671c1.178-1.169 1.908-2.789 1.908-4.58 0-.003 0-.006 0-.009z" />
+                                        <path
+                                            d="m7.412 16.592c.235.235.559.38.918.38s.683-.145.918-.38l7.342-7.342c.212-.23.341-.539.341-.878 0-.717-.582-1.299-1.299-1.299-.339 0-.647.13-.879.342l.001-.001-7.342 7.342c-.235.235-.38.559-.38.918s.145.683.38.918z" />
+                                    </svg>
+                                    <svg viewBox="0 0 24 24" on:click={share} class="w-5 h-5 md:hidden"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="m20.237 15.638c-.001 0-.002 0-.003 0-1.192 0-2.263.515-3.004 1.334l-.003.004-8.948-4.348c0-.167.084-.418.084-.669.002-.029.003-.062.003-.096 0-.176-.032-.344-.09-.499l.003.01 8.948-4.348c.744.823 1.815 1.338 3.007 1.338h.004c2.309 0 4.181-1.872 4.181-4.181s-1.872-4.181-4.181-4.181-4.181 1.872-4.181 4.181c-.002.029-.003.062-.003.096 0 .176.032.344.09.499l-.003-.01-8.948 4.348c-.744-.823-1.815-1.338-3.007-1.338-.001 0-.002 0-.004 0-2.309 0-4.181 1.872-4.181 4.181s1.872 4.181 4.181 4.181h.003c1.192 0 2.263-.515 3.004-1.334l.003-.004 8.948 4.348c0 .167-.084.418-.084.669 0 2.309 1.872 4.181 4.181 4.181s4.181-1.872 4.181-4.181c.001-.027.001-.06.001-.092 0-2.259-1.831-4.09-4.09-4.09-.032 0-.065 0-.097.001z" />
+                                    </svg>
+                                </div>
+                            </div>
+                            {#if toolTipOpen}
+                                <div class="relative">
+                                    <span
+                                        class="tooltip absolute px-6 py-2 bg-primary hidden md:block rounded  text-left -left-20 bottom-5 flex items-center justify-center z-40"
+                                        transition:fade>
+                                            Copied!
+                                    </span>
+                                </div>
+                            {/if}
 
-                    <div
-                        class="text-background bg-font py-4 px-4 mt-14 flex items-center rounded">
-                        <p class="leading-none">{generatedLink}</p>
-                    </div>
+                        </div>
+                    {/if}
                 </div>
 
                 <p class="pt-4 text-default text-center">

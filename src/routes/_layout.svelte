7dabee73917711dff8c44ddfae9ac821e29600f5
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-06-17 14:09:54 +0200

    activated analytics on deployement branch

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -98,6 +98,14 @@
     <!--Adsense-->
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=G-2X5EEDMTZE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2X5EEDMTZE');
+    </script>
 
 </svelte:head>
 

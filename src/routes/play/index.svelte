82194f6ed447e8a1ff50402b0636898f5d660b4a
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-06-23 16:44:53 +0200

    fixed merge error

--- src/routes/play/index.svelte
+++ src/routes/play/index.svelte
@@ -96,17 +96,11 @@
         name="description"
         content="Play Brawlhalla. Earn rewards. | Legit & Free In-Game objects!
         | Choose your gamemode here | Winhalla Play page" />
-<<<<<<< HEAD
-    <!--Video ads-->
     <script async src="https://cdn.stat-rock.com/player.js"></script>
-
     <link rel="canonical" href="https://winhalla.app/play" />
-
-=======
     <script
         src="https://cdn.purpleads.io/load.js?publisherId=4c614b49b1ea091717ee7674965ed444:36f81c29df2903d19389e0b048959ef43687b22b120b65ad7a71fd5759a14acce6123150f93d3b2d50d912d07d871d9b1680703a9e1af6238c5424fe2004de2b"
         id="purpleads-client"></script>
->>>>>>> master
 </svelte:head>
 {#if (!quests || (!quests.lastDaily || !quests.lastWeekly)) && (!gameModesError && !error)}
     <div>

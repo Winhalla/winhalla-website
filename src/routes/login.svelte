98c2ee6cab23c7393cdbbcdef424570cdace1c5d
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-07-02 11:50:27 +0200

    Update login.svelte

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -2,9 +2,9 @@
     import {apiUrl} from "../utils/config";
 </script>
 <svelte:head>
-    <title>Login for console and mobile players | Winhalla - Play Brawlhalla, Earn rewards.</title>
+    <title>Login | Winhalla - Play Brawlhalla, Earn rewards.</title>
 </svelte:head>
 <div>
-    <div class="text-5xl lg:text-6xl">Login for console and mobile players</div>
+    <div class="text-5xl lg:text-6xl">Login</div>
     <a href="{apiUrl}/auth/login/google" class="button button-brand">Login with Google</a>
 </div>

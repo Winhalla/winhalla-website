f9ae5baa3e256f1939b5a5f1823ad8a77e671cbf
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-07-10 15:52:38 +0200

    Revert "changed the guideCard style"
    This reverts commit 0515172e88368e91f14a6879dd942ba910dfeef0.

--- tailwind.config.js
+++ tailwind.config.js
@@ -120,7 +120,6 @@ module.exports = {
                 "screen-90": "90vh",
                 "screen-99": "99vh",
                 "screenw-99": "99vw",
-                "screenw-90": "90vw",
                 "screenw-38": "38vw",
                 "screenw-50": "50vw",
                 "10%": "10%",

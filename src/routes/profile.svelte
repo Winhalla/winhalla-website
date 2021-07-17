e25de1826517830d85c936d477a12c3a1e763b4d
23felons23 <slavikzhyhota@gmail.com>
2021-07-17 16:16:35 +0200

    Start profile page

---
+++ src/routes/profile.svelte
@@ -0,0 +1,87 @@
+<section class="flex items-center justify-between h-58 bg-variant  px-24">
+    <div>
+        <p class="text-3xl">
+            Felons
+        </p>
+        <p class="mt-1 text-mid-light">
+            Les Poros
+        </p>
+    </div>
+
+
+    <div class="text-ultra-light mt-2">
+        <p>Level: <b class="font-normal text-primary text-2xl">59</b></p>
+        <p class="mt-1">Time spent in online games: <b class="font-normal text-primary text-2xl">148h 42m 21s</b></p>
+    </div>
+</section>
+<section class="px-14 pt-12">
+    <div class="bg-variant max-w-max rounded-xl  p-8">
+        <div class="flex">
+            <div>
+                <img class="w-32" src="/assets/RankedBanners/Gold_1.png" alt="">
+
+            </div>
+            <div class="ml-6">
+                <div class="ml-2">
+                    <h3>Ranking Solo:</h3>
+                    <p class="text-primary text-3xl">GOLD 1</p>
+
+                </div>
+                <div class="ml-2 mt-4  p-4 pl-6 bg-background rounded-xl">
+                    <p class="text-lg text-mid-light">Games played: <b
+                            class="font-normal text-primary text-xl">491</b>
+                    </p>
+                    <p class="text-lg text-mid-light">Win rate: <b class="font-normal text-primary text-xl">54%</b> (<b
+                            class="font-normal text-green text-xl">251</b>/<b
+                            class="font-normal text-legendary text-xl">241</b>)</p></div>
+                <div class="ml-2 mt-5  p-4 pl-6 bg-background rounded-xl">
+                    <p class="text-lg text-mid-light  ">Current elo: <b class="font-normal text-primary text-xl">1438</b>
+                    </p>
+                    <p class="text-lg text-mid-light">Best elo: <b class="font-normal text-epic text-xl">1592</b></p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="my-8 h-px w-full bg-font"></div>
+
+        <div>
+            <h3>Best Teams:</h3>
+
+            <div class="mt-0">
+                <div class="team-container flex justify-between  my-4">
+                    <div class="flex">
+                        <img class="w-18" src="/assets/RankedIcons/Silver_1.png" alt="">
+                        <div class="ml-4">
+                            <p class="text-primary text-2xl">Silver 5</p>
+                            <p class="text-mid-light text-xl">1385 Elo</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-lg text-mid-light">With: <b class="font-normal text-font text-xl">StPersan</b></p>
+                    </div>
+                </div>
+                <div class="team-container flex justify-between  mt-8">
+                    <div class="flex">
+                        <img class="w-18" src="/assets/RankedIcons/Silver_1.png" alt="">
+                        <div class="ml-4">
+                            <p class="text-primary text-2xl">Silver 6</p>
+                            <p class="text-mid-light text-xl">1400 Elo</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-lg text-mid-light">With: <b class="font-normal text-font text-xl">Philtrom</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section></section>
+
+
+<style>
+    .team-container {
+        @apply rounded-xl;
+    }
+</style>

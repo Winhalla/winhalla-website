04bdf7a961cfd7adc4db8e6fe25e951e6b5c68a3
23felons23 <slavikzhyhota@gmail.com>
2021-07-17 18:55:27 +0200

    Add PayPal option layout

--- src/routes/shop.svelte
+++ src/routes/shop.svelte
@@ -256,6 +256,15 @@
             isBuying = false;
         }
     }
+
+    let currentAmount = 0;
+    $: if (currentAmount) {
+        handlePaypalConversion()
+    }
+    let amountToCoins = 0;
+    async function handlePaypalConversion() {
+        amountToCoins = currentAmount * 6400
+    }
 </script>
 
 <style>
@@ -447,48 +456,102 @@
                             </div>
                         </div>
                         <div class="pt-8 lg:pt-16">
+                            <div class="flex items-center">
+                                <h2  class="text-6xl text-center lg:text-left">
+                                    Paypal Credit
+                                </h2>
+                                <img class="w-24 -ml-2 -mt-2" src="/assets/Paypal_Logo.png" alt="">
+                            </div>
+
+
+                            <div class="bg-variant max-w-max rounded-xl  p-8 relative  mt-10">
+                                <p class="absolute -top-3 left-8 text-primary  text-2xl">Coin TRADER</p>
+                                <p class="text-3xl mt-4">Exchange your <b class="font-normal text-epic">coins</b> for <b class="font-normal text-epic">real money</b></p>
+                                <p class="text-mid-light">Min amount: 1$</p>
+                                <p class="mt-3">You need to have a <a href="https://www.paypal.com/" class="text-primary">Paypal account</a></p>
+                                <div class="flex items-center  mt-2">
+
+                                    <div>
+                                        <input bind:value={currentAmount} class="p-2 pl-4 text-background  rounded w-32" type="number" min="1" step="any" placeholder="Amount in $"/>
+                                    </div>
+                                    <svg class="mx-4 fill-current w-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m24 12.16-5.76-5.76v4.24h-18.24v3.04h18.24v4.24z"/></svg>
+
+
+                                    <div
+                                            on:mouseenter={() => featuredItem.tooltipOpen = true}
+                                            on:mouseleave={() => featuredItem.tooltipOpen = false}>
+                                        <button
+                                                disabled={!!featuredItem.unBuyable}
+                                                on:click={() => buyItem(featuredItem.id, featuredItem.name)}
+                                                on:mouseenter={() => featuredItem.tooltipOpen = true}
+                                                on:mouseleave={() => featuredItem.tooltipOpen = false}
+                                                class="px-4 py-1 bg-primary rounded">
+                                            <div class="flex  items-center  text-2xl">
+                                                <b
+                                                        class="mr-2 font-normal"
+                                                        style="padding-top: 0.12rem">{amountToCoins}</b>
+                                                <div class="w-8 mt-1 text-font"
+                                                     style="margin-top: 0.25rem; margin-bottom: 0.35rem">
+                                                    <CoinIcon />
+                                                </div>
+                                            </div>
+                                            {#if featuredItem.tooltipOpen && featuredItem.unBuyable}
+                                                    <span
+                                                            class="tooltip absolute bottom-15 right-1 lg:right-11 px-3 py-2 bg-legendary text-background rounded text-left flex items-center justify-center z-40"
+                                                            style="width:fit-content;"
+                                                            transition:fade>
+                                                        {featuredItem.unBuyable}
+                                                    </span>
+                                            {/if}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="pt-8 lg:pt-14 lg:pb-6">
                             <h2 class="text-6xl text-center lg:text-left">
                                 Season packs
                             </h2>
                             <div
-                                class="mt-2 flex flex-col items-center lg:flex-row lg:items-start">
+                                    class="mt-2 flex flex-col items-center lg:flex-row lg:items-start">
                                 {#if seasonPacks.forEach}
                                     {#each seasonPacks as seasonPack, i}
                                         <div
-                                            class="mx-5 mb-7 lg:ml-0 lg:mb-0 lg:mr-12 test shop-item xl:w-shopItemLarge 2xl:w-shopItem">
+                                                class="mx-5 mb-7 lg:ml-0 lg:mb-0 lg:mr-12 test shop-item xl:w-shopItemLarge 2xl:w-shopItem">
                                             <img
-                                                class="w-full h-full block "
-                                                src="assets/ShopItems/{seasonPack.name}.jpg"
-                                                alt={seasonPack.name} />
+                                                    class="w-full h-full block "
+                                                    src="assets/ShopItems/{seasonPack.name}.jpg"
+                                                    alt={seasonPack.name} />
                                             <div
-                                                class="absolute bottom-0 z-10 pl-5 pb-3 w-full">
+                                                    class="absolute bottom-0 z-10 pl-5 pb-3 w-full">
                                                 <p
-                                                    class:hidden={seasonPack.isDescriptionToggled}
-                                                    class:-mb-1={!seasonPack.isDescriptionToggled}
-                                                    class="text-accent text-4xl lg:text-5xl md:mb-0 md:block">
+                                                        class:hidden={seasonPack.isDescriptionToggled}
+                                                        class:-mb-1={!seasonPack.isDescriptionToggled}
+                                                        class="text-accent text-4xl lg:text-5xl md:mb-0 md:block">
                                                     {seasonPack.name
                                                         .toLowerCase()
                                                         .replace(/\-/g, ' ')}
                                                 </p>
                                                 <p
-                                                    class:hidden={!seasonPack.isDescriptionToggled}
-                                                    class="block xl:mt-0">
+                                                        class:hidden={!seasonPack.isDescriptionToggled}
+                                                        class="block xl:mt-0">
                                                     {seasonPack.description}
                                                 </p>
 
                                                 <div
-                                                    class="flex justify-between w-full items-end pr-4 md:pr-5 pb-1">
+                                                        class="flex justify-between w-full items-end pr-4 md:pr-5 pb-1">
                                                     <div class="-mb-2 md:mb-0">
                                                         <div>
                                                             <p
-                                                                class="hidden lg:block mr-1 -mb-2">
+                                                                    class="hidden lg:block mr-1 -mb-2">
                                                                 {seasonPack.description}
                                                             </p>
                                                             <button
-                                                                class="focus:outline-none xl:hidden -mb-10"
-                                                                on:click={() => handleDescriptionToggle(seasonPack)}>
+                                                                    class="focus:outline-none xl:hidden -mb-10"
+                                                                    on:click={() => handleDescriptionToggle(seasonPack)}>
                                                                 <p
-                                                                    class=" text-light text-lg underline leading-none">
+                                                                        class=" text-light text-lg underline leading-none">
                                                                     {seasonPack.isDescriptionToggled ? 'Hide description' : 'Show description'}
                                                                 </p>
                                                             </button>
@@ -497,13 +560,13 @@
                                                     <div on:mouseenter={() => seasonPack.tooltipOpen = true}
                                                          on:mouseleave={() => seasonPack.tooltipOpen = false}>
                                                         <button
-                                                            disabled={!!seasonPack.unBuyable}
-                                                            on:click={() => buyItem(seasonPack.id,seasonPack.name)}
-                                                            class="px-4 py-1 bg-primary rounded">
+                                                                disabled={!!seasonPack.unBuyable}
+                                                                on:click={() => buyItem(seasonPack.id,seasonPack.name)}
+                                                                class="px-4 py-1 bg-primary rounded">
                                                             <div class="flex  items-center  text-2xl">
                                                                 <b
-                                                                    class="mr-2 font-normal"
-                                                                    style="padding-top: 0.12rem">{seasonPack.cost.toLocaleString()}</b>
+                                                                        class="mr-2 font-normal"
+                                                                        style="padding-top: 0.12rem">{seasonPack.cost.toLocaleString()}</b>
                                                                 <div class="w-8 mt-1 text-font"
                                                                      style="margin-top: 0.25rem; margin-bottom: 0.35rem">
                                                                     <CoinIcon />
@@ -512,11 +575,11 @@
                                                             {#if seasonPack.tooltipOpen && seasonPack.unBuyable}
 
                                                                 <span
-                                                                    class="tooltip text-center absolute bottom-15 right-1 px-3 py-2 bg-legendary text-background rounded text-left flex items-center justify-center z-40"
-                                                                    class:tooltip={window.innerWidth < 1024}
-                                                                    class:tooltip-alt={window.innerWidth > 1024}
-                                                                    style="width:fit-content;"
-                                                                    transition:fade>
+                                                                        class="tooltip text-center absolute bottom-15 right-1 px-3 py-2 bg-legendary text-background rounded text-left flex items-center justify-center z-40"
+                                                                        class:tooltip={window.innerWidth < 1024}
+                                                                        class:tooltip-alt={window.innerWidth > 1024}
+                                                                        style="width:fit-content;"
+                                                                        transition:fade>
                                                                     {seasonPack.unBuyable}
                                                                 </span>
                                                             {/if}

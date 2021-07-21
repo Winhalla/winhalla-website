39a03072a65d53999a2b6bd885156727241f308c
23felons23 <slavikzhyhota@gmail.com>
2021-07-21 17:48:32 +0200

    build

--- __sapper__/build/server/server.js
+++ __sapper__/build/server/server.js
@@ -2677,7 +2677,7 @@ ${gameModesError && error
 				: ``}`
 			: ``}`}</div></div>
         ${validate_component(GuideCard, "GuideCard").$$render($$result, { page: "play" }, {}, {})}
-        <div class="${"mt-6"}"><script src="${"https://cdn.purpleads.io/agent.js?publisherId=4c614b49b1ea091717ee7674965ed444:36f81c29df2903d19389e0b048959ef43687b22b120b65ad7a71fd5759a14acce6123150f93d3b2d50d912d07d871d9b1680703a9e1af6238c5424fe2004de2b"}" data-pa-tag async></script></div></div>`}
+        <div class="${"mt-6"}"><script data-cfasync="${"false"}" type="${"text/javascript"}" src="${"https://www.linkonclick.com/a/display.php?r=4824847"}"></script></div></div>`}
 ${info
 	? `${validate_component(Infos, "Infos").$$render($$result, Object_1.assign(info), {}, {})}`
 	: ``}`;

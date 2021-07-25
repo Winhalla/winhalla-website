83d0dc1b50df1e936af70a022c00eccbde48ebef
23felons23 <slavikzhyhota@gmail.com>
2021-07-25 19:48:45 +0200

    Separate profile page into components + start dynamizing

---
+++ src/components/profile/GlobalStats.svelte
@@ -0,0 +1,86 @@
+<script>
+    export let data;
+    data.losses = data.games - data.wins;
+    data.winRate = (data.wins / data.games) * 100;
+    data.lossRate = 100 - data.winRate;
+
+
+</script>
+
+
+<div class="relative bg-variant max-w-max rounded-xl  p-8 ml-18 xl:ml-10   h-auto">
+    <p class="absolute -top-3 text-xl text-mid-light">GLOBAL</p>
+    <div class="flex">
+        <div class="">
+            <div class="ml-2">
+                <h3 class="font-bold text-xl" style="font-family: 'Roboto Condensed', sans-serif">Total games
+                    played:</h3>
+                <p class="text-primary text-3xl">{data.games}</p>
+
+                <!--Win rate details-->
+                <div>
+                    <div class="flex w-full  mt-4">
+                        <div class="rounded-l h-1 bg-green" style="width: {data.winRate}%"></div>
+                        <div class="rounded-r h-1 bg-legendary" style="width: {data.lossRate}%"></div>
+                    </div>
+                    <div class="flex justify-between text-xl mt-1">
+                        <p class="text-green">{data.wins} <b class="font-normal ml-1">({parseInt(data.winRate)}%)</b></p>
+                        <p class="text-legendary">{data.losses}</p>
+
+                    </div>
+                </div>
+
+
+            </div>
+            <!--Stats-->
+            <div>
+                <p class="ml-2 mt-9  text-xl ">KOs: <b
+                        class="font-normal text-epic text-2xl">{data.kos}</b>
+                <div class="ml-2 mt-1  p-4 pl-6 bg-background rounded-xl  flex">
+                    <div>
+                        <p class="text-lg text-mid-light mt-">Unarmed: <b
+                                class="font-normal text-primary text-xl">486</b>
+                        </p>
+                        <p class="text-lg text-mid-light mt-">Bomb: <b
+                                class="font-normal text-primary text-xl">63</b></p>
+                        <p class="text-lg text-mid-light mt-">Mine: <b
+                                class="font-normal text-primary text-xl">14</b></p>
+
+                    </div>
+                    <div class="ml-8">
+                        <p class="text-lg text-mid-light mt-">Weapon throw: <b
+                                class="font-normal text-primary text-xl">TBA</b></p>
+                        <p class="text-lg text-mid-light mt-">Spikeball: <b
+                                class="font-normal text-primary text-xl">27</b></p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p class="ml-2 mt-7  text-xl ">Damage: <b
+                        class="font-normal text-epic text-2xl">{data.damagedealt}</b>
+                <div class="ml-2 mt-1  p-4 pl-6 bg-background rounded-xl  flex">
+                    <div>
+                        <p class="text-lg text-mid-light mt-">Unarmed: <b
+                                class="font-normal text-primary text-xl">{data.damageunarmed}</b>
+                        </p>
+                        <p class="text-lg text-mid-light mt-">Bomb: <b
+                                class="font-normal text-primary text-xl">{data.damagebomb}</b></p>
+                        <p class="text-lg text-mid-light mt-">Mine: <b
+                                class="font-normal text-primary text-xl">{data.damagemine}</b></p>
+
+                    </div>
+                    <div class="ml-8">
+                        <p class="text-lg text-mid-light mt-">Weapon throw: <b
+                                class="font-normal text-primary text-xl">TBA</b></p>
+                        <p class="text-lg text-mid-light mt-">Spikeball: <b
+                                class="font-normal text-primary text-xl">{data.damagespikeball}</b></p>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
---
+++ src/components/profile/LegendStats.svelte
@@ -0,0 +1,125 @@
+<div class="relative bg-variant min-w-md rounded-xl  p-8    h-auto" style="min-width: 22rem;">
+    <p class="absolute -top-3 text-xl text-mid-light">LEGENDS</p>
+
+    <div class="ml-2 mt-1  bg-background p-6 pt-4  rounded-xl">
+        <div class="flex items-center">
+            <img class="w-12 mr-3" src="/assets/LegendIcons/ada.png" alt="">
+            <select class="w-full bg-background p-3 pl-4  rounded-xl  border-4 border-variant     focus:outline-none"
+                    name="Choose a Weapon">
+                <option value="">Ada</option>
+                <option class="" value="">
+                    Nix
+                </option>
+                <option value="">Orion</option>
+                <option value="">Petra</option>
+                <option value="">Wu Shang</option>
+            </select>
+
+
+        </div>
+        <div class="mt-6 ml-2  text-xl  ">
+            <p class="text-mid-light mt-">Time played: <b class="  font-normal text-primary text-2xl">18h
+                10m 36s</b>
+            </p>
+            <p class="text-mid-light mt-">Level: <b class="  font-normal text-primary text-2xl">28</b>
+            </p>
+            <p class="text-mid-light mt-3">Games played: <b
+                    class="font-normal text-primary text-2xl">54</b>
+            </p>
+            <p class="text-mid-light mt-">Win rate: <b class="font-normal text-primary text-2xl">
+                58%</b>
+            </p>
+            <p class="text-mid-light mt-3">KOs: <b class="font-normal text-primary text-2xl">330</b>
+            </p>
+            <p class="text-mid-light mt-">Damage dealt: <b class="font-normal text-primary text-2xl">16845</b>
+            </p>
+        </div>
+    </div>
+
+
+    <div class="mt-14 ml-2 w-full">
+
+        <div class="flex text-xl  -mt-2  ">
+            <div class="flex items-center  text-lg">
+                <select class="bg-background p-3  rounded-lg     focus:outline-none" name="Choose a Weapon"
+                        id="">
+                    <option class="" value="">Win rate</option>
+                    <option value="">Time played</option>
+                    <option value="">Games Played</option>
+                    <option value="">KOs</option>
+                    <option value="">Damage</option>
+
+                </select>
+            </div>
+
+            <div class="flex items-center ml-5 text-lg">
+                <p class="mr-2">Display:</p>
+                <input class="w-12  bg-background p-3 pr-0 rounded-lg" value="2" type="number" min="1" step="1">
+            </div>
+        </div>
+        <div class="w-full  mt-5  p-4 pl-6 bg-background rounded-xl">
+            <button class="w-full flex items-center">
+
+                <div>
+                    <img class="w-10" src="/assets/LegendIcons/ada.png" alt="">
+                </div>
+                <div class="flex justify-between w-full">
+                    <p class="ml-3">ADA</p>
+                    <p class="text-green">
+                        58%
+                    </p>
+                </div>
+
+                <div class="ml-3 /-mt-2 text-3xl text-light" style="font-family: sans-serif">
+                    +
+                </div>
+            </button>
+
+            <!--<div class="mt-4">
+                <p class="text-lg text-mid-light mt-">Time played: <b class="font-normal text-primary text-xl">28h
+                    24m 31s</b>
+                </p>
+                <p class="text-lg text-mid-light mt-">Games played: <b
+                        class="font-normal text-primary text-xl">54</b>
+                </p>
+                <p class="text-lg text-mid-light mt-">KOs: <b class="font-normal text-primary text-xl">68</b>
+                </p>
+                <p class="text-lg text-mid-light mt-">Damage dealt: <b class="font-normal text-primary text-xl">16845</b>
+                </p>
+            </div>-->
+        </div>
+        <div class="w-full  mt-3  p-4 pl-6 bg-background rounded-xl">
+            <button class="w-full flex items-center">
+
+                <div>
+                    <img class="w-10" src="/assets/LegendIcons/nix.png" alt="">
+                </div>
+                <div class="flex justify-between w-full">
+                    <p class="ml-3">NIX</p>
+                    <p class="text-green">
+                        52%
+                    </p>
+                </div>
+
+                <div class="ml-3 /-mt-2 text-3xl text-light" style="font-family: sans-serif">
+                    +
+                </div>
+            </button>
+
+            <!--<div class="mt-4">
+                <p class="text-lg text-mid-light mt-">Time played: <b class="font-normal text-primary text-xl">28h
+                    24m 31s</b>
+                </p>
+                <p class="text-lg text-mid-light mt-">Games played: <b
+                        class="font-normal text-primary text-xl">54</b>
+                </p>
+                <p class="text-lg text-mid-light mt-">KOs: <b class="font-normal text-primary text-xl">68</b>
+                </p>
+                <p class="text-lg text-mid-light mt-">Damage dealt: <b class="font-normal text-primary text-xl">16845</b>
+                </p>
+            </div>-->
+        </div>
+
+    </div>
+
+</div>

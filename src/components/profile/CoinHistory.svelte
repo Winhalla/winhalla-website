5b92a7b6c9a4f2613628349c46ef56087dda4d1d
23felons23 <slavikzhyhota@gmail.com>
2021-07-31 23:11:58 +0200

    Add Winhalla stats page base layout

---
+++ src/components/profile/CoinHistory.svelte
@@ -0,0 +1,49 @@
+<script>
+    import CoinIcon from "../CoinIcon.svelte";
+    import addSuffixTo from "../../utils/addSuffixTo";
+</script>
+
+<div class="relative bg-variant  rounded-2xl   p-8  mt-12 xl:ml-20   h-auto w-1/2">
+    <p class="absolute -top-3 text-xl text-mid-light">HISTORY</p>
+
+    <div class="-mt-1  w-full">
+        <div class="flex justify-between  p-4 pl-6 bg-background rounded-xl  text-xl  mt-3">
+
+            <div class="flex">
+                <div class="flex items-center  mr-6 text-epic">
+                    <p class=" text-2xl">10</p>
+                    <div class="ml-1 mb-1  w-6">
+                        <CoinIcon/>
+                    </div>
+                </div>
+                <p class="text-default  font-bold mt-px" style="font-family: 'Roboto Condensed', sans-serif;">Daily
+                    Quest</p>
+
+
+                <p class="ml-28 text-primary text-2xl">"Do 25 KOs"</p>
+            </div>
+
+            <p class="text-mid-light text-2xl">10:32</p>
+        </div>
+
+
+        <div class="flex justify-between  p-4 pl-6 bg-background rounded-xl  text-xl  mt-3">
+
+            <div class="flex">
+                <div class="flex items-center  mr-6 text-epic">
+                    <p class=" text-2xl">24</p>
+                    <div class="ml-1 mb-1  w-6">
+                        <CoinIcon/>
+                    </div>
+                </div>
+                <p class="text-default  font-bold mt-px" style="font-family: 'Roboto Condensed', sans-serif;">Solo match</p>
+
+
+                <p class="ml-28 text-primary text-2xl">{addSuffixTo(1)} place</p>
+            </div>
+
+            <p class="text-mid-light text-2xl">9:46</p>
+        </div>
+    </div>
+
+</div>
---
+++ src/components/profile/CoinStats.svelte
@@ -0,0 +1,54 @@
+<div class="relative bg-variant  rounded-2xl   p-8  mt-12 xl:ml-5   h-auto w-full md:w-auto">
+    <p class="absolute -top-3 text-xl text-mid-light">COINS</p>
+
+    <div class="md:ml-2">
+        <div class="">
+            <h3 class="font-bold text-default" style="font-family: 'Roboto Condensed', sans-serif">Total coins earned:</h3>
+            <p class="text-primary text-3xl">23054</p>
+        </div>
+
+        <div class="flex  mt-6">
+            <div class="">
+                <h3 class="text-xl" >SOLO coins:</h3>
+                <p class="text-xl    text-epic"><strong class="font-normal  text-2xl" style="margin-right: 0.15rem">1548</strong>  (82%)</p>
+
+                <div class="mt-2  p-4 pl-6 bg-background rounded-xl  text-lg">
+                    <p class="text-mid-light mt-">Games played: <b
+                            class="font-normal text-primary text-xl">178</b>
+                    </p>
+                    <p class="text-mid-light mt-">Games won: <b
+                            class="font-normal text-primary text-xl">71</b>
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="ml-17">
+                <h3 class="text-xl" >Quests coins:</h3>
+                <p class="text-xl    text-legendary"><strong class="font-normal  text-2xl" style="margin-right: 0.15rem">1548</strong>  (14%)</p>
+
+                <div class="mt-2  p-4 pl-6 bg-background rounded-xl  text-lg">
+                    <p class="text-mid-light mt-">From dailies: <b
+                            class="font-normal text-primary text-xl">2604</b>
+                    </p>
+                    <p class="text-mid-light mt-">From weeklies: <b
+                            class="font-normal text-primary text-xl">1866</b>
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="ml-17">
+                <h3 class="text-xl" >Referral link coins:</h3>
+                <p class="text-xl    text-accent"><strong class="font-normal  text-2xl" style="margin-right: 0.15rem">1548</strong>  (6%)</p>
+
+                <div class="mt-2  p-4 pl-6 bg-background rounded-xl  text-lg">
+                    <p class="text-mid-light mt-">Friends invited: <b
+                            class="font-normal text-primary text-xl">6</b>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>

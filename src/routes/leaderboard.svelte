59d5bbf41c0657e234fc062af501f7bd9a853e73
23felons23 <slavikzhyhota@gmail.com>
2021-08-07 22:18:28 +0200

    b

---
+++ src/routes/leaderboard.svelte
@@ -0,0 +1,14 @@
+<script>
+import CoinIcon from "../components/CoinIcon.svelte";
+</script>
+
+<div class="ml-24 mt-14">
+    <h1 class="text-6xl">Weekly Leaderboard</h1>
+    <div class="flex">
+        <h2>Total reward:</h2>
+        <p class="ml-1  text-primary">6000</p>
+        <div class="ml-1  w-6 text-primary">
+            <CoinIcon/>
+        </div>
+    </div>
+</div>

761b73ef78b8e7923b3b670a2c6a6f1908d4efb5
23felons23 <slavikzhyhota@gmail.com>
2021-08-20 23:27:37 +0200

    update service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -17,7 +17,7 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET" || event.request.headers.has("range")) return;
+    if (event.request.method !== "GET" || event.request.headers.has("range") || event.request.host !== "winhalla.app") return;
 
     const url = new URL(event.request.url);
 

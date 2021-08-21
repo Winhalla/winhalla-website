a043984a1e6918a2aa964f9f89e2e6dd32607491
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-08-21 13:49:34 +0200

    b

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,55 +1,37 @@
 <svelte:head>
-    <script async src="https://serving.stat-rock.com/player.js"></script>
+    <script async src="https://cdn.stat-rock.com/player.js"></script>
 </svelte:head>
 <div>
-    <script data-playerPro="current">
-        function playAd() {
-            const init = (api) => {
-                if (api) {
+    <script>
+        function init(api) {
+            if (api) {
+                api.on("AdStarted", function() {
+                        console.log("AdStarted");
+                    }
+                );
 
-                    api.on("AdVideoStart", function() {
-                        document.getElementById("transfer").value = 1;
-                        document.getElementById("transfer").dispatchEvent(new CustomEvent("input"));
-                        if (document.location.hostname === "winhalla.app") api.setAdVolume(1);
-                        document.body.onblur = function() {
-                            //api.pauseAd();
-                        };
-                        document.body.onfocus = function() {
-                            //api.resumeAd();
-                        };
-                    });
-                    api.on("AdVideoFirstQuartile", () => {
-                        document.getElementById("transfer").value = 2;
-                        document.getElementById("transfer").dispatchEvent(new CustomEvent("input"));
-                    });
-                    api.on("AdVideoMidpoint", () => {
-                        document.getElementById("transfer").value = 3;
-                        document.getElementById("transfer").dispatchEvent(new CustomEvent("input"));
-                    });
-                    api.on("AdVideoThirdQuartile", () => {
-                        document.getElementById("transfer").value = 4;
-                        document.getElementById("transfer").dispatchEvent(new CustomEvent("input"));
-                    });
-                    api.on("AdVideoComplete", function() {
-                        document.getElementById("transfer").value = 5;
-                        document.getElementById("transfer").dispatchEvent(new CustomEvent("input"));
-                        document.body.onblur = null;
-                        document.body.onfocus = null;
-                    });
-                } else {
-                    console.log("blank");
-                }
-            };
-            var s = document.querySelector("script[data-playerPro=\"current\"]");
-            //s.removeAttribute("data-playerPro");
-            (playerPro = window.playerPro || []).push({
-                id: "oOMhJ7zhhrjUgiJx4ZxVYPvrXaDjI3VFmkVHIzxJ2nYvXX8krkzp",
-                after: s,
-                init: init
-            });
+
+                api.on("AdVideoComplete", function() {
+                        console.log("AdVideoComplete");
+                    }
+                );
+            } else {
+                console.log("blank");
+            }
         }
     </script>
+
+    <script data-playerPro="current">(function() {
+        var s = document.querySelector("script[data-playerPro=\"current\"]");
+        s.removeAttribute("data-playerPro");
+        (playerPro = window.playerPro || []).push({
+            id: "oOMhJ7zhhrjUgiJx4ZxVYPvrXaDjI3VFmkVHIzxJ2nYvXX8krkzp",
+            after: s,
+            init: init
+        });
+    })();
+    </script>
 </div>
-<button onclick="playAd()" class="button button-brand">
+<button onclick="init()" class="button button-brand">
     Play ad
 </button>

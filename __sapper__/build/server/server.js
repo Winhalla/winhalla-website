d364c2cb76e188c562d56e047b0737f0f3682c22
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-08-25 17:17:25 +0200

    b

--- __sapper__/build/server/server.js
+++ __sapper__/build/server/server.js
@@ -4005,12 +4005,13 @@ ${gameModesError && error
 						{},
 						{}
 					)}
-                            <div class="${"mt-10"}"><script data-playerPro="${"current"}">(function() {
+                            <div class="${"mt-10 flex"}"><script data-playerPro="${"current"}">(function() {
                                     var s = document.querySelector("script[data-playerPro=\\"current\\"]");
                                     s.removeAttribute("data-playerPro");
                                     (playerPro = window.playerPro || []).push({ id: "7rYaA2Kc71uu", after: s });
                                 })();
-                                </script></div></div>`
+                                </script>
+                                <script src="${"https://cdn.purpleads.io/agent.js?publisherId=4c614b49b1ea091717ee7674965ed444:36f81c29df2903d19389e0b048959ef43687b22b120b65ad7a71fd5759a14acce6123150f93d3b2d50d912d07d871d9b1680703a9e1af6238c5424fe2004de2b"}" data-pa-tag async></script></div></div>`
 				: ``}`
 			: ``}`}</div></div>
         ${validate_component(GuideCard, "GuideCard").$$render($$result, { page: "play" }, {}, {})}</div>`}

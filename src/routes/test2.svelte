ca2147f611b0c420121c3450ce442f0bcf2e9a2c
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-08-25 13:22:44 +0200

    b

--- src/routes/test2.svelte
+++ src/routes/test2.svelte
@@ -1,9 +1,11 @@
 <svelte:head>
     <script async src="https://cdn.stat-rock.com/player.js"></script>
 </svelte:head>
-<script data-playerPro="current">(function() {
-    var s = document.querySelector("script[data-playerPro=\"current\"]");
-    s.removeAttribute("data-playerPro");
-    (playerPro = window.playerPro || []).push({ id: "7rYaA2Kc71uu", after: s });
-})();
-</script>
+<div>
+    <script data-playerPro="current">(function() {
+        var s = document.querySelector("script[data-playerPro=\"current\"]");
+        s.removeAttribute("data-playerPro");
+        (playerPro = window.playerPro || []).push({ id: "7rYaA2Kc71uu", after: s });
+    })();
+    </script>
+</div>

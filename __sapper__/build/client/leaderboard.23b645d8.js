cec0d2356a536c3013d54bd2e37aef82c70354ed
23felons23 <slavikzhyhota@gmail.com>
2021-09-01 11:41:17 +0200

    b

---
+++ __sapper__/build/client/leaderboard.23b645d8.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as l}from"./client.44a93c32.js";export default class extends s{constructor(s){super(),t(this,s,null,null,l,{})}}
--- __sapper__/build/client/leaderboard.a8689f13.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as e,s as t}from"./client.840ee27d.js";export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}
